2f8e70bd633723ab41667cf89f9ae0c9adf54bbf
Tim Skauge <tim@skauge.dk>
2013-07-23 20:28:44 +0200

    removed x-powered-by

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.disable('x-powered-by');
 
 // development only
 if ('development' == app.get('env')) {
