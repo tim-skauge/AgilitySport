2b706578f2cc645161852f62373dee2a372ba2c5
Tim Skauge <tim@skauge.dk>
2013-07-23 20:34:08 +0200

    changed greeting message when server is started

--- app.js
+++ app.js
@@ -30,5 +30,5 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('Server listening on port ' + app.get('port'));
 });
