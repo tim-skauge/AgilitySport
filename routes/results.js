ea022e98e0c7c4db4279ee4942dbebcb3ccbcc31
Tim Skauge <tim@skauge.dk>
2013-07-25 17:30:58 +0200

    added sample data and updated layout

--- routes/results.js
+++ routes/results.js
@@ -1,3 +1,9 @@
-exports.single = function(req, res){
-  res.render('result');
+var moment = require('moment')
+  , sampleData = require('./data.json');
+
+exports.single = function(req, res) {
+  sampleData.createdFormatted = moment(sampleData.created).format('LL');
+  console.log(sampleData);
+
+  res.render('result', sampleData);
 };
