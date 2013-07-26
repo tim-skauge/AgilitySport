24e568b9c642f208c97297ea07fe9f1ba0934236
Tim Skauge <tim@skauge.dk>
2013-07-26 08:46:10 +0200

    added time faults and calculated total faults

--- routes/results.js
+++ routes/results.js
@@ -3,7 +3,14 @@ var moment = require('moment')
 
 exports.single = function(req, res) {
   sampleData.createdFormatted = moment(sampleData.created).format('LL');
-  console.log(sampleData);
+
+  // calculate time faults
+  for (var i in sampleData.results) {
+    var result = sampleData.results[i];
+
+    result.timeFaults = Math.max(result.time - sampleData.courseTimeInSeconds, 0);
+    result.totalFaults = result.courseFaults + result.timeFaults;
+  }
 
   sampleData.title = "Results for " + sampleData.className + ", " + sampleData.dogSize + ". " + sampleData.competition + " " + sampleData.createdFormatted;
   res.render('result', sampleData);
